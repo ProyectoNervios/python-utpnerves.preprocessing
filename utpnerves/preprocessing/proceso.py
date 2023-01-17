28417ddcf0dba92f987d48c14f3c8fdcfe308bde
Yeison Cardona <yencardonaal@unal.edu.co>
2023-01-17 08:43:36 -0500

    NO TABS

--- utpnerves/preprocessing/proceso.py
+++ utpnerves/preprocessing/proceso.py
@@ -18,281 +18,281 @@ Image = TypeVar('Image', bound=np.ndarray)
 
 class Proceso:
 
-	def limitar(self, im: Image) -> Image:
-		"""
-		Esta función permite acotar una imagen en el rango
-		de 0 a 255. Esto se hace ncesario ya que al aplicar
-		algunas funciones de opencv o de alguna librería de
-		procesamiento de imágenes, la imagen resultante puede
-		encontrarse en rangos comprendidos entre 0 y 255.
+    def limitar(self, im: Image) -> Image:
+        """
+        Esta función permite acotar una imagen en el rango
+        de 0 a 255. Esto se hace ncesario ya que al aplicar
+        algunas funciones de opencv o de alguna librería de
+        procesamiento de imágenes, la imagen resultante puede
+        encontrarse en rangos comprendidos entre 0 y 255.
 
-		Parameters
-		----------
-		im
-		   Imagen de un Canal, por ejemplo R del RGB.
+        Parameters
+        ----------
+        im
+           Imagen de un Canal, por ejemplo R del RGB.
 
-		Returns
-		-------
-		im
-			Imagen acotada
-		"""
-		
-		Mayores = im > 255
-		im[Mayores] = 255
-		Menores = im < 0
-		im[Menores] = 0
-		return im
+        Returns
+        -------
+        im
+            Imagen acotada
+        """
 
-
-	def imadjust(
-			self, imagen: Image, low_in: float, hig_in: float, 
-			low_out: float, hig_out: float, gamma: float) -> Image:
-		"""
-		Esta función permite aplicar una transformación
-		lineal a una imagen(canal). Esta ecuación se encuentra
-		especificada en el texto de Gonzales Wood 
-		"Digital Image Processing".
-
-		Parameters
-		----------
-		imagen
-			Imagen de un canal, por ejemplo R del RGB.
-		low_in
-			El valor más pequeño que tiene la imagen(canal) original.
-		hig_in
-			El valor más grande que tiene la imagen(canal) original.
-		low_out
-			El valor más pequeño que desea mapear la imagen original.
-		hig_out
-			El valor más grande que desea mapear la imagen original.
-		gamma
-			Valor que controla el brillo de la imagen resultante.
-
-		Returns
-		-------
-		imagen_mod
-			Imagen con la transformación lineal aplicada.
-		"""
-
-		imagen_mod = np.zeros(imagen.shape, dtype=np.uint8)
-		imagen_mod = (
-			low_out 
-			+ (hig_out-low_out) 
-			* ((imagen-low_in) / (hig_in-low_in))**gamma)
-
-		imagen_mod = np.round(255 * imagen_mod)
-		imagen_mod = self.limitar(imagen_mod)
-		imagen_mod = np.uint8(imagen_mod)
-
-		return imagen_mod
+        Mayores = im > 255
+        im[Mayores] = 255
+        Menores = im < 0
+        im[Menores] = 0
+        return im
 
 
-	def white_balance(self, image: Image) -> Image:
-		"""
-		Esta función permite mapear una imagen a sus valores extremos.
-		Es posible encontrar este algortimo implementado en muchos programas
-		como GIMP o ADOBE PHOTOSHOP. Los valores de cada variable que se
-		especifica en la función se extrajeron de la función "imadjust"
-		que emplea Matlab.
+    def imadjust(
+            self, imagen: Image, low_in: float, hig_in: float,
+                        low_out: float, hig_out: float, gamma: float) -> Image:
+        """
+        Esta función permite aplicar una transformación
+        lineal a una imagen(canal). Esta ecuación se encuentra
+        especificada en el texto de Gonzales Wood
+        "Digital Image Processing".
 
-		Parameters
-		----------
-		imagen
-			Imagen original con los 3 canales, por ejemplo RGB.
+        Parameters
+        ----------
+        imagen
+            Imagen de un canal, por ejemplo R del RGB.
+        low_in
+            El valor más pequeño que tiene la imagen(canal) original.
+        hig_in
+            El valor más grande que tiene la imagen(canal) original.
+        low_out
+            El valor más pequeño que desea mapear la imagen original.
+        hig_out
+            El valor más grande que desea mapear la imagen original.
+        gamma
+            Valor que controla el brillo de la imagen resultante.
 
-		Returns
-		-------
-			Imagen con la transformación balance de blancos.
-		"""
+        Returns
+        -------
+        imagen_mod
+            Imagen con la transformación lineal aplicada.
+        """
 
-		canal = image.astype('float') / 255.0
-		min_canal = np.percentile(canal, 1)
-		max_canal = np.percentile(canal, 99)
-		canal_balance = self.imadjust(canal, min_canal, max_canal, 0.0, 1.0, 1)
-		return canal_balance
+        imagen_mod = np.zeros(imagen.shape, dtype=np.uint8)
+        imagen_mod = (
+                    low_out
+                        + (hig_out-low_out)
+                        * ((imagen-low_in) / (hig_in-low_in))**gamma)
 
+        imagen_mod = np.round(255 * imagen_mod)
+        imagen_mod = self.limitar(imagen_mod)
+        imagen_mod = np.uint8(imagen_mod)
 
-	def manipulacion_datos_prediccion(self, imagen: Image) -> Image:
-		"""
-		Esta función permite realizar el procesamiento necesario
-		para que la imagen sea leída por el modelo en la etapa
-		de inferencia. Primero se cambian las dimensiones 
-		de la imagen y se transforma en un tensor y después se
-		realiza un escalamiento.
-		
-		Parameters
-		----------
-		imagen
-			Imagen canal, por ejemplo R del RGB.
-
-		Returns
-		-------
-		imagen
-			Imagen mapeada al rango [0,1] y en forma de tensor.
-		"""
-
-		logging.debug('-' * 60)
-		logging.debug('Loading and preprocessing train data...')
-		logging.debug('-' * 60)
-
-		imagen = imagen.reshape((1,
-								 imagen.shape[0],
-								 imagen.shape[1], 1))
-		imagen = imagen / 255.
-		return imagen
+        return imagen_mod
 
 
-	def imagen_proceso(self, imagen):
-		"""
-		Esta función implementa otras funciones
-		definidas anteriormente para obtener
-		el procesamiento definitivo de las imagenes:
-		Balance de Blancos, Reducción del tamaño de
-		una imagen, Mapeo de la imagen al rango [0,1] y 
-		la transformación de la imagen en un tensor.
-		
-		Parameters
-		----------
-		imagen
-			Imagen en los 3 canales, por ejemplo RGB.
+    def white_balance(self, image: Image) -> Image:
+        """
+        Esta función permite mapear una imagen a sus valores extremos.
+        Es posible encontrar este algortimo implementado en muchos programas
+        como GIMP o ADOBE PHOTOSHOP. Los valores de cada variable que se
+        especifica en la función se extrajeron de la función "imadjust"
+        que emplea Matlab.
 
-		Returns
-		-------
-		imagen
-			Imagen procesada.
-		"""
-		balance = self.white_balance(imagen)
-		resized_imagen = cv.resize(
-			balance, (320, 180), 
-			interpolation=cv.INTER_AREA)
-		resized_imagen = np.array(resized_imagen)
-		imagenTensor = self.manipulacion_datos_prediccion(resized_imagen)
-		return imagenTensor
+        Parameters
+        ----------
+        imagen
+            Imagen original con los 3 canales, por ejemplo RGB.
+
+        Returns
+        -------
+            Imagen con la transformación balance de blancos.
+        """
+
+        canal = image.astype('float') / 255.0
+        min_canal = np.percentile(canal, 1)
+        max_canal = np.percentile(canal, 99)
+        canal_balance = self.imadjust(canal, min_canal, max_canal, 0.0, 1.0, 1)
+        return canal_balance
 
 
-	def aumento_tam(self, imagen: Image, tam_nuevo: tuple) -> Image:
-		"""
-		Esta función permite modificar el tamaño de la imagen.
-		Este proceso es necesario ya que se requiere volver
-		al tamaño original para analizar de forma visual
-		el resultado obtenido.
-		
-		Parameters
-		----------
-		imagen
-			Imagen canal, por ejemplo R del RGB.
-		tam_nuevo
-			Tupla que define el nuevo ancho y largo de la imagen.
+    def manipulacion_datos_prediccion(self, imagen: Image) -> Image:
+        """
+        Esta función permite realizar el procesamiento necesario
+        para que la imagen sea leída por el modelo en la etapa
+        de inferencia. Primero se cambian las dimensiones
+        de la imagen y se transforma en un tensor y después se
+        realiza un escalamiento.
 
-		Returns
-		-------
-		resized_imagen
-			Imagen ampliada.
-		"""
-		resized_imagen = cv.resize(
-			imagen,
-			(tam_nuevo[1], tam_nuevo[0]),
-			interpolation=cv.INTER_AREA)
-		# resized_imagen = Limitar(resized_imagen)
-		return resized_imagen
+        Parameters
+        ----------
+        imagen
+            Imagen canal, por ejemplo R del RGB.
 
+        Returns
+        -------
+        imagen
+            Imagen mapeada al rango [0,1] y en forma de tensor.
+        """
 
-	def remover_areas(
-			self, 
-			imagen: Image, 
-			min_size: int = 500) -> Image:
-		"""
-		Función que permite mejorar el resultado de la segmentación 
-		o predicción, removiendo aquellas zonas que sumen cierto valor.
+        logging.debug('-' * 60)
+        logging.debug('Loading and preprocessing train data...')
+        logging.debug('-' * 60)
 
- 		Parameters
-		----------
-		imagen
-			Imagen binaria.
-		min_size
-			Valor del área que se quiere remover. El área se define
-			como la cantidad de pixeles que son iguales a 1. El valor 
-			por defecto es 500, lo que quiere decir se van a remover 
-			todos los objetos de la imagen que sumen igual o menos de 
-			500 píxeles.
-
-		Returns
-		-------
-		im_result
-			Imagen binaria filtrada.
-		"""
-		nb_blobs, im_with_separated_blobs, stats, _ = cv.connectedComponentsWithStats(np.uint8(imagen))
-		sizes = stats[:, -1]
-		sizes = sizes[1:]
-		nb_blobs -= 1
-		im_result = np.zeros(imagen.shape)
-
-		for blob in range(nb_blobs):
-			if sizes[blob] >= min_size:
-				# see description of im_with_separated_blobs above
-				im_result[im_with_separated_blobs == blob + 1] = 1.0
-		return im_result
+        imagen = imagen.reshape((1,
+                                         imagen.shape[0],
+                                                                 imagen.shape[1], 1))
+        imagen = imagen / 255.
+        return imagen
 
 
-	def cuadrar_rect(self, mascara: Image) -> Image:
-		"""
-		Esta función permite calcular el rectángulo mas pequeño
-		que encierra el objeto.
+    def imagen_proceso(self, imagen):
+        """
+        Esta función implementa otras funciones
+        definidas anteriormente para obtener
+        el procesamiento definitivo de las imagenes:
+        Balance de Blancos, Reducción del tamaño de
+        una imagen, Mapeo de la imagen al rango [0,1] y
+        la transformación de la imagen en un tensor.
 
-		Parameters
-		----------
-		mascara
-			Imagen binaria.
+        Parameters
+        ----------
+        imagen
+            Imagen en los 3 canales, por ejemplo RGB.
 
-		Returns
-		-------
-			Imagen binaria (Rectángulo).
-		"""
-		img = np.array(mascara, dtype = np.uint8)
-		imgRGB = cv.cvtColor(img.copy(), cv.COLOR_GRAY2RGB)
-		a, ctrs = cv.findContours(
-			img, 
-			cv.RETR_EXTERNAL, 
-			cv.CHAIN_APPROX_SIMPLE)
-
-		# print(a)
-		boxes = []
-		for ctr in a:
-			x, y, w, h = cv.boundingRect(ctr)
-			boxes.append([x, y, w, h])
-
-		for box in boxes:
-			top_left = (box[0], box[1])
-			bottom_right = (box[0] + box[2], box[1] + box[3])
-			# para que quede solo el recuadro, cambiar el -1 por un 2
-			cv.rectangle(
-				imgRGB, top_left, bottom_right, 
-				(255, 255, 255), -1)
-
-		return imgRGB[:, :, 0] / 255.0
+        Returns
+        -------
+        imagen
+            Imagen procesada.
+        """
+        balance = self.white_balance(imagen)
+        resized_imagen = cv.resize(
+                    balance, (320, 180),
+                        interpolation=cv.INTER_AREA)
+        resized_imagen = np.array(resized_imagen)
+        imagenTensor = self.manipulacion_datos_prediccion(resized_imagen)
+        return imagenTensor
 
 
-	def dim_rec(self, mascara: Image, imagen: Image) -> Image:
-		"""
-		Función para crear la máscara rectángulo.
+    def aumento_tam(self, imagen: Image, tam_nuevo: tuple) -> Image:
+        """
+        Esta función permite modificar el tamaño de la imagen.
+        Este proceso es necesario ya que se requiere volver
+        al tamaño original para analizar de forma visual
+        el resultado obtenido.
 
-		Parameters
-		----------
-		mascara
-			Imagen binaria.
-		imagen 
-			Imagen canal, por ejemplo R del RGB.
+        Parameters
+        ----------
+        imagen
+            Imagen canal, por ejemplo R del RGB.
+        tam_nuevo
+            Tupla que define el nuevo ancho y largo de la imagen.
 
-		Returns
-		-------
-		mask
-			Porción de la imagen.
-		"""
-		cnts, _ = cv.findContours(
-			np.uint8(mascara), cv.RETR_EXTERNAL, 
-			cv.CHAIN_APPROX_SIMPLE)
-		mask = imagen[
-			cnts[0][0][0][1] : cnts[0][1][0][1], 
-			cnts[0][0][0][0] : cnts[0][2][0][0]]
-		return mask
+        Returns
+        -------
+        resized_imagen
+            Imagen ampliada.
+        """
+        resized_imagen = cv.resize(
+                    imagen,
+                        (tam_nuevo[1], tam_nuevo[0]),
+                        interpolation=cv.INTER_AREA)
+        # resized_imagen = Limitar(resized_imagen)
+        return resized_imagen
+
+
+    def remover_areas(
+            self,
+                        imagen: Image,
+                        min_size: int = 500) -> Image:
+        """
+        Función que permite mejorar el resultado de la segmentación
+        o predicción, removiendo aquellas zonas que sumen cierto valor.
+
+        Parameters
+        ----------
+        imagen
+            Imagen binaria.
+        min_size
+            Valor del área que se quiere remover. El área se define
+            como la cantidad de pixeles que son iguales a 1. El valor
+            por defecto es 500, lo que quiere decir se van a remover
+            todos los objetos de la imagen que sumen igual o menos de
+            500 píxeles.
+
+        Returns
+        -------
+        im_result
+            Imagen binaria filtrada.
+        """
+        nb_blobs, im_with_separated_blobs, stats, _ = cv.connectedComponentsWithStats(np.uint8(imagen))
+        sizes = stats[:, -1]
+        sizes = sizes[1:]
+        nb_blobs -= 1
+        im_result = np.zeros(imagen.shape)
+
+        for blob in range(nb_blobs):
+            if sizes[blob] >= min_size:
+                # see description of im_with_separated_blobs above
+                im_result[im_with_separated_blobs == blob + 1] = 1.0
+        return im_result
+
+
+    def cuadrar_rect(self, mascara: Image) -> Image:
+        """
+        Esta función permite calcular el rectángulo mas pequeño
+        que encierra el objeto.
+
+        Parameters
+        ----------
+        mascara
+            Imagen binaria.
+
+        Returns
+        -------
+            Imagen binaria (Rectángulo).
+        """
+        img = np.array(mascara, dtype = np.uint8)
+        imgRGB = cv.cvtColor(img.copy(), cv.COLOR_GRAY2RGB)
+        a, ctrs = cv.findContours(
+                    img,
+                        cv.RETR_EXTERNAL,
+                        cv.CHAIN_APPROX_SIMPLE)
+
+        # print(a)
+        boxes = []
+        for ctr in a:
+            x, y, w, h = cv.boundingRect(ctr)
+            boxes.append([x, y, w, h])
+
+        for box in boxes:
+            top_left = (box[0], box[1])
+            bottom_right = (box[0] + box[2], box[1] + box[3])
+            # para que quede solo el recuadro, cambiar el -1 por un 2
+            cv.rectangle(
+                            imgRGB, top_left, bottom_right,
+                                (255, 255, 255), -1)
+
+        return imgRGB[:, :, 0] / 255.0
+
+
+    def dim_rec(self, mascara: Image, imagen: Image) -> Image:
+        """
+        Función para crear la máscara rectángulo.
+
+        Parameters
+        ----------
+        mascara
+            Imagen binaria.
+        imagen
+            Imagen canal, por ejemplo R del RGB.
+
+        Returns
+        -------
+        mask
+            Porción de la imagen.
+        """
+        cnts, _ = cv.findContours(
+                    np.uint8(mascara), cv.RETR_EXTERNAL,
+                        cv.CHAIN_APPROX_SIMPLE)
+        mask = imagen[
+                    cnts[0][0][0][1] : cnts[0][1][0][1],
+                        cnts[0][0][0][0] : cnts[0][2][0][0]]
+        return mask
 
--- utpnerves/preprocessing/unet.py
+++ utpnerves/preprocessing/unet.py
@@ -3,9 +3,9 @@
 Unet
 ====
 
-Este archivo contiene funciones para el cálculo del coeficiente Dice como 
-métrica y como función de costo, y para la construcción de modelos de red 
-neuronal U-Net. 
+Este archivo contiene funciones para el cálculo del coeficiente Dice como
+métrica y como función de costo, y para la construcción de modelos de red
+neuronal U-Net.
 """
 
 import numpy as np
@@ -15,165 +15,165 @@ from tensorflow.keras.layers import Conv2DTranspose, MaxPooling2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import backend as K
 
+
 class Unet:
 
-	def dice_coef(
-			self, y_true: np.ndarray, 
-			y_pred: np.ndarray) -> float:
-		"""
-		Cálculo del coeficiente Dice como métrica.
+    def dice_coef(
+        self, y_true: np.ndarray,
+            y_pred: np.ndarray) -> float:
+        """
+        Cálculo del coeficiente Dice como métrica.
 
-		Parameters
-		----------
-		y_true
-			Etiquetas.
-		y_pred
-			Predicciones.
+        Parameters
+        ----------
+        y_true
+            Etiquetas.
+        y_pred
+            Predicciones.
 
-		Returns
-		-------
-		float
-			Coeficiente dice.
-		"""
+        Returns
+        -------
+        float
+            Coeficiente dice.
+        """
 
-		# Metrica de evalaución
-		smooth = 1
-		y_true_f = K.flatten(y_true)
-		y_pred_f = K.flatten(y_pred)
-		intersection = K.sum(y_true_f * y_pred_f)
-		return (
-			(2.*intersection + smooth) / (K.sum(y_true_f) 
-			+ K.sum(y_pred_f)+smooth))
+        # Metrica de evalaución
+        smooth = 1
+        y_true_f = K.flatten(y_true)
+        y_pred_f = K.flatten(y_pred)
+        intersection = K.sum(y_true_f * y_pred_f)
+        return (
+            (2. * intersection + smooth) / (K.sum(y_true_f) +
+                                            K.sum(y_pred_f) + smooth))
 
+    # Función de costo
 
-	# Función de costo
-	def dice_coef_loss(
-			self, y_true: np.ndarray, 
-			y_pred: np.ndarray) -> float:
-		"""
-		Cálculo del coeficiente Dice como funcion de costo.
+    def dice_coef_loss(
+        self, y_true: np.ndarray,
+            y_pred: np.ndarray) -> float:
+        """
+        Cálculo del coeficiente Dice como funcion de costo.
 
-		Parameters
-		----------
-		y_true
-			Etiquetas.
-		y_pred
-			Predicciones.
+        Parameters
+        ----------
+        y_true
+            Etiquetas.
+        y_pred
+            Predicciones.
 
-		Returns
-		-------
-		float
-			Coeficiente dice.
-		"""
+        Returns
+        -------
+        float
+            Coeficiente dice.
+        """
 
-		return -self.dice_coef(y_true, y_pred)
+        return -self.dice_coef(y_true, y_pred)
 
+    def unet(self) -> Model:
+        """
+        Implementación del modelo de red neuronal U-Net.
 
-	def unet(self) -> Model:
-		"""
-		Implementación del modelo de red neuronal U-Net.
+        Returns
+        -------
+        model
+            Modelo de red neuronal U-Net.
+        """
 
-		Returns
-		-------
-		model
-			Modelo de red neuronal U-Net.
-		"""
+        # Etapa de contracción
+        entrada = Input((180, 320, 1))
 
-		# Etapa de contracción
-		entrada = Input((180, 320, 1))
+        # Bloque 1
+        conv1 = Conv2D(
+            32, (3, 3), activation='relu', padding='same')(entrada)
+        conv1 = Conv2D(
+            32, (3, 3), activation='relu', padding='same')(conv1)
+        pool1 = MaxPooling2D((2, 2), strides=2, padding='same')(conv1)
+        drop1 = Dropout(0.2)(pool1)
+        # Bloque 2
+        conv2 = Conv2D(
+            64, (3, 3), activation='relu', padding='same')(drop1)
+        conv2 = Conv2D(
+            64, (3, 3), activation='relu', padding='same')(conv2)
+        pool2 = MaxPooling2D((2, 2), strides=2, padding='same')(conv2)
+        drop2 = Dropout(0.2)(pool2)
+        # Bloque 3
+        conv3 = Conv2D(
+            128, (3, 3), activation='relu', padding='same')(drop2)
+        conv3 = Conv2D(
+            128, (3, 3), activation='relu', padding='same')(conv3)
+        pool3 = MaxPooling2D(
+            (2, 2), strides=(5, 2), padding='same')(conv3)
+        drop3 = Dropout(0.2)(pool3)
+        # Bloque 4
+        conv4 = Conv2D(
+            256, (3, 3), activation='relu', padding='same')(drop3)
+        conv4 = Conv2D(
+            256, (3, 3), activation='relu', padding='same')(conv4)
+        pool4 = MaxPooling2D(
+            (2, 2), strides=(3, 2), padding='same')(conv4)
+        drop4 = Dropout(0.2)(pool4)
 
-		# Bloque 1
-		conv1 = Conv2D(
-			32, (3, 3), activation='relu', padding='same')(entrada)
-		conv1 = Conv2D(
-			32, (3, 3), activation='relu', padding='same')(conv1)
-		pool1 = MaxPooling2D((2, 2), strides=2, padding='same')(conv1)
-		drop1 = Dropout(0.2)(pool1)
-		# Bloque 2
-		conv2 = Conv2D(
-			64, (3, 3), activation='relu', padding='same')(drop1)
-		conv2 = Conv2D(
-			64, (3, 3), activation='relu', padding='same')(conv2)
-		pool2 = MaxPooling2D((2, 2), strides=2, padding='same')(conv2)
-		drop2 = Dropout(0.2)(pool2)
-		# Bloque 3
-		conv3 = Conv2D(
-			128, (3, 3), activation='relu', padding='same')(drop2)
-		conv3 = Conv2D(
-			128, (3, 3), activation='relu', padding='same')(conv3)
-		pool3 = MaxPooling2D(
-			(2, 2), strides=(5, 2), padding='same')(conv3)
-		drop3 = Dropout(0.2)(pool3)
-		# Bloque 4
-		conv4 = Conv2D(
-			256, (3, 3), activation='relu', padding='same')(drop3)
-		conv4 = Conv2D(
-			256, (3, 3), activation='relu', padding='same')(conv4)
-		pool4 = MaxPooling2D(
-			(2, 2), strides=(3, 2), padding='same')(conv4)
-		drop4 = Dropout(0.2)(pool4)
+        # Enlace o puente
+        conv5 = Conv2D(
+            512, (3, 3), activation='relu', padding='same')(pool4)
+        conv5 = Conv2D(
+            512, (3, 3), activation='relu', padding='same')(conv5)
 
-		# Enlace o puente
-		conv5 = Conv2D(
-			512, (3, 3), activation='relu', padding='same')(pool4)
-		conv5 = Conv2D(
-			512, (3, 3), activation='relu', padding='same')(conv5)
+        # Etapa de expansión
+        # Bloque 5
+        exp1 = Conv2DTranspose(
+            256, (2, 2), strides=(3, 2), padding='same',
+            activation='relu')(conv5)
+        cont1 = concatenate([exp1, conv4])
+        conv6 = Conv2D(
+            256, (3, 3), activation='relu', padding='same')(cont1)
+        conv6 = Conv2D(
+            256, (3, 3), activation='relu', padding='same')(conv6)
+        # Bloque 6
+        exp2 = Conv2DTranspose(
+            128, (2, 2), strides=(5, 2), padding='same',
+            activation='relu')(conv6)
+        cont2 = concatenate([exp2, conv3])
+        conv7 = Conv2D(
+            128, (3, 3), activation='relu', padding='same')(cont2)
+        conv7 = Conv2D(
+            128, (3, 3), activation='relu', padding='same')(conv7)
+        # Bloque 7
+        exp3 = Conv2DTranspose(
+            64, (2, 2), strides=(2, 2), padding='same',
+            activation='relu')(conv7)
+        cont3 = concatenate([exp3, conv2])
+        conv8 = Conv2D(
+            64, (3, 3), activation='relu', padding='same')(cont3)
+        conv8 = Conv2D(
+            64, (3, 3), activation='relu', padding='same')(conv8)
+        # Bloque 8
+        exp4 = Conv2DTranspose(
+            32, (2, 2), strides=(2, 2), padding='same',
+            activation='relu')(conv8)
+        cont4 = concatenate([exp4, conv1])
+        conv9 = Conv2D(
+            32, (3, 3), activation='relu', padding='same')(cont4)
+        conv9 = Conv2D(
+            64, (3, 3), activation='relu', padding='same')(conv9)
 
-		# Etapa de expansión
-		# Bloque 5
-		exp1 = Conv2DTranspose(
-			256, (2, 2), strides=(3, 2), padding='same',
-			activation='relu')(conv5)
-		cont1 = concatenate([exp1, conv4])
-		conv6 = Conv2D(
-			256, (3, 3), activation='relu', padding='same')(cont1)
-		conv6 = Conv2D(
-			256, (3, 3), activation='relu', padding='same')(conv6)
-		# Bloque 6
-		exp2 = Conv2DTranspose(
-			128, (2, 2), strides=(5, 2), padding='same',
-			activation='relu')(conv6)
-		cont2 = concatenate([exp2, conv3])
-		conv7 = Conv2D(
-			128, (3, 3), activation='relu', padding='same')(cont2)
-		conv7 = Conv2D(
-			128, (3, 3), activation='relu', padding='same')(conv7)
-		# Bloque 7
-		exp3 = Conv2DTranspose(
-			64, (2, 2), strides=(2, 2), padding='same',
-			activation='relu')(conv7)
-		cont3 = concatenate([exp3, conv2])
-		conv8 = Conv2D(
-			64, (3, 3), activation='relu', padding='same')(cont3)
-		conv8 = Conv2D(
-			64, (3, 3), activation='relu', padding='same')(conv8)
-		# Bloque 8
-		exp4 = Conv2DTranspose(
-			32, (2, 2), strides=(2, 2), padding='same',
-			activation='relu')(conv8)
-		cont4 = concatenate([exp4, conv1])
-		conv9 = Conv2D(
-			32, (3, 3), activation='relu', padding='same')(cont4)
-		conv9 = Conv2D(
-			64, (3, 3), activation='relu', padding='same')(conv9)
+        # Etapa de organización. Practicamente no cambia y es obligatoria en casi
+        # todas las redes FCN y por supuesto va siempre al final
+        conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
 
-		# Etapa de organización. Practicamente no cambia y es obligatoria en casi
-		# todas las redes FCN y por supuesto va siempre al final
-		conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+        # Juntamos el modelo. Le estamos diciendo desde donde inicia el modelo
+        # y hasta donde va.
+        model = Model(inputs=[entrada], outputs=[conv10])
 
-		# Juntamos el modelo. Le estamos diciendo desde donde inicia el modelo
-		# y hasta donde va.
-		model = Model(inputs=[entrada], outputs=[conv10])
+        # Compilamos el modelo
+        model.compile(
+            optimizer=Adam(learning_rate=1e-5),
+            loss=self.dice_coef_loss,
+            metrics=[self.dice_coef])
 
-		# Compilamos el modelo
-		model.compile(
-			optimizer=Adam(learning_rate=1e-5),
-			loss = self.dice_coef_loss,
-			metrics = [self.dice_coef])
+        # model.compile(optimizer='adam',
+        #              loss='binary_crossentropy',
+        #              metrics=['accuracy'])
 
-		# model.compile(optimizer='adam',
-		#              loss='binary_crossentropy',
-		#              metrics=['accuracy'])
-
-		return model
+        return model
 
